a23ca1520d84f2e03029f46c80170302d63daa72
Andre Kristensen <andrek1994@gmail.com>
2026-03-12 17:53:35 +0100

    Lagt til endelig bilde av Andre på team

--- src/components/om-optit/Team.tsx
+++ src/components/om-optit/Team.tsx
@@ -16,7 +16,7 @@ export function Team() {
               {/* Firkantet bilde */}
               <div className="aspect-square relative bg-slate-800 w-full">
                 <Image
-                  src="/images/team/Andre Louis Kristensen - OptIT.jpg"
+                  src="/images/team/André Louis Kristensen - OptIT_komp.jpg"
                   alt="André Louis Kristensen, Grunder av OptIT, daglig leder og spesialist innen systemutvikling"
                   fill
                   className="object-cover"
